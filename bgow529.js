34cf0c42d4d8cd38245ab10480cb2c43a0736065
Bhargava <bgow529@aucklanduni.ac.nz>
2019-08-03 18:40:59 +1200

    display almost finished

--- bgow529.js
+++ bgow529.js
@@ -1,4 +1,42 @@
 const topNav = document.getElementById("top_nav");
+const xhr = new XMLHttpRequest();
+
+displayAllItems("");
+
+
+function displayAllItems(searchTerm = ""){
+    console.log("displaying");
+    let container = document.getElementById("display container");
+    xhr.open("GET","http://redsox.uoa.auckland.ac.nz/ms/MuseumService.svc/search?term="+searchTerm,true);
+    xhr.setRequestHeader("Accept","application/json;charset=utf-8");
+    xhr.onload = () => {
+        let allItems = JSON.parse(xhr.responseText);
+        console.log("items = " + allItems);
+        let allItemsHtml = allItems.map(displayItem);
+        allItemsHtml.forEach( htmlItem => {container.appendChild(htmlItem)});
+    }
+    xhr.send();
+    
+
+}
+
+function displayItem(itemObject){
+    let mainDiv = document.createElement("div");
+    mainDiv.className = "display item";
+    let heading = document.createElement("h3");
+    heading.innerHTML = itemObject.Title;
+    let desciption = document.createElement("p");
+    desciption.innerHTML = itemObject.Description;
+    imgurl = "http://redsox.uoa.auckland.ac.nz/ms/MuseumService.svc/itemimg?id=" + itemObject.ItemId;
+    let displayImg = document.createElement("img");
+    displayImg.src = imgurl;
+    mainDiv.appendChild(heading);
+    mainDiv.appendChild(displayImg);
+    mainDiv.appendChild(desciption);
+    console.log(heading);
+    return mainDiv;
+
+}
 
 function hideThis(thing){
     thing.style.display = "none";
